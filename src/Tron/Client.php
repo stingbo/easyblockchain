45a48fa6ccf6b9827150c1a33ac249e59502ed36
sting_bo <lianbo.wan@gmail.com>
2023-01-22 23:48:31 +0800

    简化波场客户端请求，增加请求TRONGRID API功能

--- src/Tron/Client.php
+++ src/Tron/Client.php
@@ -6,13 +6,13 @@ use EasyBlockchain\Tron\Kernel\BaseClient;
 
 class Client extends BaseClient
 {
-    public function get(string $uri, array $param = [])
+    public function get(string $url, array $query = [], string $sign_type = 'NONE')
     {
-        return $this->httpGet($uri, $param);
+        return $this->httpGet($url, $query, $sign_type);
     }
 
-    public function postJson(string $uri, array $params = [])
+    public function postJson(string $url, array $data = [], array $query = [], string $sign_type = 'NONE')
     {
-        return $this->httpPostJson($uri, $params);
+        return $this->httpPostJson($url, $data, $query, $sign_type);
     }
 }
